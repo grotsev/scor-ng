47072a4f61bf9d9cf9b954908769071635b15814
grotsev <denis.grotsev@gmail.com>
2017-12-05 15:02:41 +0300

    Add selenide

---
+++ selenide/test/GoogleTest.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.CollectionCondition.size;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+
+public class GoogleTest {
+  @Test
+  public void search_selenide_in_google() {
+    open("http://google.com/ncr");
+    $(By.name("q")).val("selenide").pressEnter();
+    $$("#ires .g").shouldHave(size(10));
+    $("#ires .g").shouldBe(visible).shouldHave(
+        text("Selenide: concise UI tests in Java"),
+        text("selenide.org"));
+  }
+}
